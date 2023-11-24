ca56ce03f3e0eb9899368dc8d5cf6c01a2a6056b
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-11-24 17:48:09 -0300

    chore: include lint os posts in User

--- src/main/java/com/workshopmongo/domain/User.java
+++ src/main/java/com/workshopmongo/domain/User.java
@@ -1,21 +1,23 @@
 package com.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Document(collection = "user")
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
+@Document(collection = "user")
 public class User implements Serializable {
   private static final long serialVersionUID = 1L;
 
@@ -24,4 +26,14 @@ public class User implements Serializable {
   private String name;
   @Indexed(unique = true)
   private String email;
+  @Setter(AccessLevel.NONE)
+  @DBRef(lazy = true)
+  private List<Post> posts = new ArrayList<>();
+
+  public User(String id, String name, String email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+
 }

2d4d17102ad8dc8f9351d044c8ec1b1f80de89d1
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-11-24 20:23:52 -0300

    feat: implements find by title for posts

--- src/main/java/com/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/workshopmongo/repositories/PostRepository.java
@@ -1,7 +1,9 @@
 package com.workshopmongo.repositories;
 
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+  List<Post> findByTitleContainingIgnoreCase(String text);
 }
